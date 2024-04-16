cacec8951fb74d164d83f4702c0ea64900a18db5
“rezan5910” <bytemefirst@gmail.com>
2024-04-16 10:00:42 +0100

    doc:comments on fun

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import { Client} from "discord.js"
 import ready from './listeners/ready';
 
 const token = process.env.TOKEN;
-if (!token) {
-    console.error('No token provided');
-    process.exit(1);
-}
 console.log('FACTROID IS starting .....')
 const client = new Client({
     intents: []
--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,6 +1,11 @@
 import { Client } from "discord.js";
-
+/**
+ * sepreating and registering a listner to listen to 
+ * variety of messages, user activities  etc
+ * 
+ */
 export default (client: Client): void => {
+    // :void as its non value returning func
     client.on("ready", async () => {
         if (!client.user || !client.application) {
             return;
