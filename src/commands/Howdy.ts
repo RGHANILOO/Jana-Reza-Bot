97801cd78b181c47c418b791f1700ade62f18eb5
“rezan5910” <bytemefirst@gmail.com>
2024-04-16 11:10:38 +0100

    feat: /Howdy slash command

---
+++ src/commands/Howdy.ts
@@ -0,0 +1,17 @@
+import { CommandInteraction, Client , ApplicationCommandType} from "discord.js";
+import { Command } from "../Command";
+
+export const Howdy: Command = {
+    name: "howdy",
+    description: "Howdy command",
+    type: ApplicationCommandType.ChatInput,
+    run: async (client: Client, interaction: CommandInteraction) => {
+        const content = "Howdy!👋 How are you doing today?";
+
+        await interaction.followUp({
+            //https://discordjs.guide/slash-commands/response-methods.html#ephemeral-responses
+            ephemeral: true,
+            content
+        });
+    }
+}
