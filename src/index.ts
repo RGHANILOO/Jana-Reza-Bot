4358ae90fdd6aff4684dc31628be482f8a41a92a
“rezan5910” <bytemefirst@gmail.com>
2024-04-16 10:21:52 +0100

    feat: interaction creator func and slash comand func foundation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { Client} from "discord.js"
 import ready from './listeners/ready';
+import interactionCreator from './listeners/interactionCreator';
 
 const token = process.env.TOKEN;
 console.log('FACTROID IS starting .....')
@@ -9,5 +10,6 @@ const client = new Client({
 
 });
 
-ready(client);
+ready(client); // registering the listener
+interactionCreator(client); //listerning to interactipon 
 client.login(token);
