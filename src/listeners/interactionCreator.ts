45ae388a48cd7760de845c108ec2e6bfef16eb93
“rezan5910” <bytemefirst@gmail.com>
2024-04-16 15:28:55 +0100

    fix:pottential type error issue

--- src/listeners/interactionCreator.ts
+++ src/listeners/interactionCreator.ts
@@ -3,13 +3,12 @@ import { Commands } from "../commandCenter/Commands";
 
 export default (client: Client): void => {
     client.on("interactionCreate", async (interaction: Interaction) => {
-        if (interaction.isCommand() || interaction.isContextMenuCommand()) {
+        if (interaction.isCommand()) {
             await handleSlashCommand(client, interaction)
         }
     })
 }
 const handleSlashCommand = async (client: Client, interaction: CommandInteraction): Promise<void> => {
-    // this is where the slash command will be handled
     const slashCommand = Commands.find(command => command.name === interaction.commandName);
     if (!slashCommand) {
         interaction.followUp({content: 'we havea problem'})
