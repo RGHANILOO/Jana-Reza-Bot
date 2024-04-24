5becf77c505084132986f4f1a7cfb1c767ae5b2e
Janett Gerrard <mikanchik@gmail.com>
2024-04-24 11:42:17 +0100

    write party command

---
+++ src/commandCenter/commands/Partify.ts
@@ -0,0 +1,47 @@
+import {
+    CommandInteraction,
+    Client,
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
+} from 'discord.js'
+import { Command } from '../../Command'
+import { stringToTime } from '../utils/stringToTime'
+import dayjs from 'dayjs'
+import { fetchOpenAIChatCompletion } from '../../openai'
+
+const partySystemPrompt = "You are specialised in generating fun party ideas and games that are doable over voice chats like discord. When prompted by a user you will respond with a JSON object containing a name and description of the party."
+
+export const Party: Command = {
+    name: 'partify',
+    description: 'start a party!',
+    type: ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: 'theme',
+            description: 'what kind of party? theme? context?',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        },
+        {
+            name: 'when',
+            description:
+                "a time like 'tomorrow at 4pm' or 'next week sunday at 12'",
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        },
+    ],
+    run: async (_: Client, interaction: CommandInteraction) => {
+      console.log(interaction.options.get("theme")?.value)
+      const timeInput = interaction.options.get("when")?.value as string
+      const targetTime = stringToTime(timeInput, interaction.createdAt)
+
+      const generatedPartyIdea = await fetchOpenAIChatCompletion(interaction.options.get("theme")?.value as string, partySystemPrompt)
+      console.log(JSON.parse(generatedPartyIdea))
+
+      // code to generate event goes here
+
+      await interaction.followUp({
+        content: `OK! You've got a party coming up at <t:${dayjs(targetTime).unix()}>`
+      })
+    },
+}
